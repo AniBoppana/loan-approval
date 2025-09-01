bffdb61a3e3ae4f46d04807f4e06497f4e63a250
Ani <64718589+AniBoppana@users.noreply.github.com>
2025-09-01 16:27:13 -0700

    Update exploration_queries.sql

--- sql/exploration_queries.sql
+++ sql/exploration_queries.sql
@@ -1,1 +1,11 @@
-
+-- 1. Loan approval rate by ethnicity 
+SELECT 
+    applicant_ethnicity_1,
+    SUM(CASE
+        WHEN action_taken = 1 THEN 1
+        ELSE 0
+    END) * 1.0 / COUNT(*) AS approval_rate,
+    COUNT(*) AS total_applications
+FROM
+    cali_2022
+GROUP BY applicant_ethnicity_1;
